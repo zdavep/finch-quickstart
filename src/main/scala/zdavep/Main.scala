564e077d8fce6c3e3908d3a59bcad54248c93518
Dave Pederson <dave.pederson@gmail.com>
2015-05-12 23:45:48 -0600

    Add better test

--- src/main/scala/zdavep/Main.scala
+++ src/main/scala/zdavep/Main.scala
@@ -6,11 +6,11 @@ import io.finch.{Endpoint => _, _}
 import zdavep.quickstart._
 
 object Main extends App {
+  val server = Httpx.serve(":8080", Quickstart.backend)
+  val _ = Await.ready(server)
+}
 
+object Quickstart {
   val version = "v1"
   val backend = handleExceptions ! quickstartEndpoints(version)
-
-  val server = Httpx.serve(":8080", backend)
-  val _ = Await.ready(server)
-
 }
--- src/test/scala/FinchQuickstartSpec.scala
+++ src/test/scala/FinchQuickstartSpec.scala
@@ -1,8 +1,32 @@
+import com.twitter.finagle.Service
+import com.twitter.finagle.httpx.Request
+import com.twitter.util.Await
+import io.finch._
+import org.jboss.netty.handler.codec.http.{DefaultHttpRequest, HttpMethod, HttpVersion}
 import org.scalatest._
 import org.scalatest.Matchers
+import zdavep.Quickstart
 
 class FinchQuickstartSpec extends FlatSpec with Matchers {
-  "FinchQuickstart" should "have tests" in {
-    true shouldBe true
+
+  def awaitResultOf(service: Service[HttpRequest, HttpResponse]): Request => HttpResponse =
+    (req) => Await.result(service(req))
+
+  val await = awaitResultOf(Quickstart.backend)
+
+  def request(r: DefaultHttpRequest): Request = new Request {
+      val httpRequest = r
+      lazy val remoteSocketAddress = new java.net.InetSocketAddress(0)
+  }
+
+  def GET(path: String): Request =
+    request(new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, path))
+
+  "Quickstart Service" should "allow GET on greeting endpoint" in {
+    await(GET("/quickstart/api/v1/greeting/Finch")).status shouldBe io.finch.response.Ok().status
+  }
+
+  it should "return NotFound status for unknown route: /foo/bar" in {
+    await(GET("/foo/bar")).status shouldBe io.finch.response.NotFound.status
   }
 }
