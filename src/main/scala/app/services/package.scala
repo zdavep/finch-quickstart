6ac2bca20586749c45eaeb99db3f301dcbde7302
Dave Pederson <dave.pederson@gmail.com>
2015-10-04 17:08:57 -0600

    Add illegalArgs usage

--- src/main/scala/app/services/package.scala
+++ src/main/scala/app/services/package.scala
@@ -1,6 +1,7 @@
 package app
 
 import app.concurrent.inNewThread
+import app.errors.illegalArgs
 import app.models.Greeting
 import com.twitter.util.Future
 
@@ -19,9 +20,10 @@ package object services {
   /**
    * Multi-greeting service
    */
-  def greetings(name: String, n: Int, title: Option[String]): Future[Seq[Greeting]] = inNewThread {
-    val msg = title.fold(s"Hello, $name")(title => s"Hello, $title $name")
-    (1 to n).map(i => Greeting.decorate(s"$msg: $i")).toSeq
-  }
+  def greetings(name: String, n: Int, title: Option[String]): Future[Seq[Greeting]] =
+    if (n < 0 || n > 99) illegalArgs("Bad number; 0 < n < 100") else inNewThread {
+      val msg = title.fold(s"Hello, $name")(title => s"Hello, $title $name")
+      (1 to n).map(i => Greeting.decorate(s"$msg: $i")).toSeq
+    }
 }
 
